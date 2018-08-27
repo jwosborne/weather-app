e706d4fe46cbca2995172cb72771d86b2f5e720b
John Osborne <johnosborne1456@gmail.com>
2018-08-27 11:10:32 -0400

    handles user inpt through yargs and provides error checking for the request

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const request = require('request');
 const yargs = require('yargs');
 
-const argv = yargs;
+const argv = yargs
+    .options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address)
 
 request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=OZXW5Ls6PxUh6VNlIaj1wxSNOQGA3pte&location=1301%20lombard%20street%20philadelphia',
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=OZXW5Ls6PxUh6VNlIaj1wxSNOQGA3pte&location=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.results[0].providedLocation.location}`);
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lat}`);
+    if (error) {
+        console.log('Unable to connect to MapQuest servers.');
+    } else if (body.info.statuscode === 400) {
+        console.log('User did not provide an address.');
+    } else {
+        console.log(`Address: ${body.results[0].providedLocation.location}`);
+        console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+        console.log(`Longitude: ${body.results[0].locations[0].latLng.lat}`);
+    }
 });
